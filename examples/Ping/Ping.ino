c3ad1f7224ebed428a58e844356bd8105b448db6
Matthew Kendall <mdkendall@users.noreply.github.com>
2022-09-08 15:42:05 -0700

    Add example

---
+++ examples/Ping/Ping.ino
@@ -0,0 +1,28 @@
+
+#include <WiFi.h>
+#include <Thingpings.h>
+
+/* Replace with your local wireless network credentials */
+const char* ssid = "example-network";
+const char* pass = "example-password";
+
+void setup() {
+
+    /* Configure serial port for debug */
+    Serial.begin(115200);
+
+    /* Connect to Wi-Fi */
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+    Serial.print("Connecting to WiFi ");
+    while (WiFi.status() != WL_CONNECTED) { delay(100); }
+    Serial.println(WiFi.localIP());
+
+    /* Send a ping to Thingpings */
+    Thingpings::ping("ExampleCompany", "ExampleWidget");
+    Serial.println("Sent ping to Thingpings");
+}
+
+void loop() {
+
+}
